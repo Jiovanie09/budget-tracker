2744e55f09eae6f5c928bf6d1c4a8123e239d253
Jiovanie Martinez <jiovaniemartinez@gmail.com>
2020-05-24 16:12:26 -0500

    added service worker file with boiler code needed

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,73 @@
-console.log("hello from service worker!")
+// storing all the files the app needs //
+const FILES_TO_CACHE = [
+    "/",
+    "/public/index.html",
+    "/public/icons/icon-192x192.png",
+    "/public/icons/icon-512x512.png",
+    "/public/index.js",
+    "/manifest.webmanifest",
+    "/styles.css"
+];
+
+// storing static file and data using caps because never going to change// 
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// install // 
+self.addEventListener("install", function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log("your files were pre-cached successfully!");
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+    self.skipWaiting();
+});
+// activate to update old versions // 
+self.addEventListener("activate", function (event) {
+    event.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("removing old cache data", kay);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    )
+    self.Clients.claim();
+})
+
+// fetch //
+self.addEventListener("fetch", function (event) {
+    //caches successful to the api //
+    if (event.request.url.includes("/api/")) {
+
+    
+    event.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+            return fetch(event.request)
+                .then(response => {
+                    // if the response was good, clone it and store it in the cache. //
+                    if (response.status === 200) {
+                        cache.put(event.request.url, response.clone());
+                    }
+                    return response;
+                })
+                .catch(err => {
+                    return cache.match(event.request);
+                });
+        }).catch(err => console.log(err))
+    )
+    return;
+    }
+    event.respondWith(
+        caches.open(CACHE_NAME).then(cache => {
+          return cache.match(event.request).then(response => {
+            return response || fetch(event.request);
+          });
+        })
+      );
+})
